a9162dccda2f904cf89ee60ee8e5ddfee5d76613
kingdomofpomry <bnar_taieb@outlook.com>
2025-12-29 01:06:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-sw.js
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
